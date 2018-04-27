ded47d44465c66e9f5d2de174680098d49888fcc
alert8@qq.com <alert8@qq.com>
2018-04-27 16:13:16 +0800

    添加第二章 内容。

--- src/main/kotlin/com/example/knight/c2/CDPlayer.kt
+++ src/main/kotlin/com/example/knight/c2/CDPlayer.kt
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 
-
+@Component
 class CDPlayer @Autowired
 constructor(private val cd: CompactDisc) : MediaPlayer {
 
